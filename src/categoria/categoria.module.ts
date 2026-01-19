b6f345e83de2dc6eb142aa55274ec9651dfc0604
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-01-19 15:40:01 -0300

    create crud categoria

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,9 +1,9 @@
-// src/categoria/categoria.module.ts
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Categoria } from "./entities/categoria.entity"; // Importe a Categoria
-import { CategoriaService } from "./services/categoria.service";
-import { CategoriaController } from "./controllers/categoria.controller";
+import { Categoria } from "./entities/categoria.entity";
+import { CategoriaService } from "./service/categoria.service";
+import { CategoriaController } from "./controller/categoria.controller";
+
 
 @Module({
     imports: [TypeOrmModule.forFeature([Categoria])], 
